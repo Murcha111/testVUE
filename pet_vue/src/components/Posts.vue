801fd832fb78ce5a179bb575d80ffde68fa75cad
Murcha111 <lenaev84@gmail.com>
2022-09-25 21:34:49 +0300

    create posts

---
+++ pet_vue/src/components/Posts.vue
@@ -0,0 +1,27 @@
+<template>
+  
+  <div v-for="post in posts" class="post" v-bind:key="post.id" >
+      <div> <strong>Название:</strong>  {{ post.title}}</div>
+      <div> <strong>Описание:</strong> {{ post.body}}</div>
+  </div>
+</template>
+
+<script>
+  export default{
+    props:{
+      posts: {
+        type: Array,
+        required: true
+      }
+
+    }
+  }
+  
+</script>
+
+<style scoped>
+.post{
+  border: 2px rgb(95, 140, 95) solid;
+  margin-top: 10px;
+  }
+</style>
